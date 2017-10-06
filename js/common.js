d372f3c839d8b51afd0d49523cf59a149f8a28a9
MATEY15 <act1987@mail.ru>
2017-10-07 01:18:12 +0300

    First footer

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
-	// Custom JS
+	// Footer height
+
+	function resize() {
+		var foot = document.getElementById('footer');
+		var footHeight = foot.offsetHeight;
+		//foot.style.marginTop = -footHeight+"px";
+		document.getElementById('body').style.paddingBottom = footHeight+"px";
+	}
+	window.onload = resize;
 
 });
