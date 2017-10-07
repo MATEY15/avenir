311f0e6606793aa8c0322b95f29f44b551c3b928
MATEY15 <act1987@mail.ru>
2017-10-08 01:04:36 +0300

    Mobile Menu

--- js/common.js
+++ js/common.js
@@ -12,17 +12,38 @@ $(function() {
 		var foot = document.getElementById('footer');
 		var footHeight = foot.offsetHeight;
 		//foot.style.marginTop = -footHeight+"px";
-		document.getElementById('body').style.paddingBottom = footHeight+"px";
+		document.getElementById('body').style.paddingBottom = footHeight+'px';
 	}
 	window.onresize = resize;
 	window.onload = resize;
 
 	// Open Menu
 
-	$(".container").click(function() {
-    $(".stick").toggleClass(function () {
+	$('.mobile-menu__open').click(function() {
+    $('.stick').toggleClass(function () {
       return $(this).is('.open, .close') ? 'open close' : 'open';
     });
+  });
+
+	// Top Line Bacground
+
+  $('.mobile-menu__open').click(function(){
+  	$('.top-line__wrapper').toggleClass('top-line__wrapper--bgt top-line__wrapper--bg');
+  });
+
+  // Open mobile menu
+
+  $('.mobile-menu__open').click(function(){
+    if ($('.header-nav__list').css('display') == 'none') 
+        { 
+            $('.mobile-menu__list').animate({height: 'show'}, 300); 
+        } 
+    else 
+        {     
+            $('.mobile-menu__list').animate({height: 'hide'}, 300); 
+        }
+
+  	//$('.mobile-menu__list').animate({height: 'show'}, 500); 
   });
 
 });
