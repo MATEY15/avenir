c5524057fa57a8b7492be54161130455ae31287a
MATEY15 <act1987@mail.ru>
2017-10-11 14:09:31 +0300

    about and services

--- js/common.js
+++ js/common.js
@@ -17,14 +17,14 @@ $(function() {
 
 	// Footer height
 
-	function resize() {
-		var foot = document.getElementById('footer');
-		var footHeight = foot.offsetHeight;
-		//foot.style.marginTop = -footHeight+"px";
-		document.getElementById('body').style.paddingBottom = footHeight+'px';
-	}
-	window.onload = resize;
-	window.onresize = resize;
+	// function resize() {
+	// 	var foot = document.getElementById('footer');
+	// 	var footHeight = foot.offsetHeight;
+	// 	//foot.style.marginTop = -footHeight+"px";
+	// 	document.getElementById('body').style.paddingBottom = footHeight+'px';
+	// }
+	// window.onload = resize;
+	// window.onresize = resize;
 
 	// Fixed menu
 
